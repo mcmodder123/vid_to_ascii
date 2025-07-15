aca5d57a7996831205e26eedafcb985ee0cd5e0d
Juan Manuel Rodriguez <juanmodder1@gmail.com>
2025-07-15 10:47:41 -0500

    made error handling in src/main.rs more detailed
    used Result<(), Box<dyn Error>> to make current errors more detailed

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 // Video to ASCII converter
 // Written by Juan Manuel Rodriguez (mcmodder123)
 
-use std::env;
+use std::{env, error::Error};
 use vid_to_ascii::{args, video};
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let args = env::args().collect();
     let video = args::parse_args(args);
-    let result = video::play_video(&video);
-    if let Err(e) = result {
-        eprintln!("Failed to play video: {}", e);
+    if let Err(e) = video::play_video(&video) {
+        eprintln!("Failed to play video:");
+        return Err(e);
     }
+    Ok(())
 } // main.rs
