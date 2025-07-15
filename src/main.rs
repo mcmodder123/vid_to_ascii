f529c0ad164af607cbd77973c0be3394c808b767
Juan Manuel Rodriguez <juanmodder1@gmail.com>
2025-07-15 10:29:39 -0500

    added even better error handling

--- src/main.rs
+++ src/main.rs
@@ -7,5 +7,8 @@ use vid_to_ascii::{args, video};
 fn main() {
     let args = env::args().collect();
     let video = args::parse_args(args);
-    let _ = video::play_video(&video);
+    let result = video::play_video(&video);
+    if let Err(e) = result {
+        eprintln!("Failed to play video: {}", e);
+    }
 } // main.rs
